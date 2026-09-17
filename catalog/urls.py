cf56e1b8c4288baa96fb096d4c65c8ed82002829
Dalton Bowling <dbowl456@gmail.com>
2026-09-16 21:14:02 -0400

    Prep 3

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,12 +4,16 @@ import pathlib
 # this is in locallibrary/catalog/urls.py which I *just* created
 # the current file is definitely NOT locallibrary/locallibrary_config/urls.py!
 _this_dir = pathlib.Path(__file__).resolve().parent
-assert not (_this_dir / "settings.py").exists(), "catalog/urls.py should NOT have a sibling named settings.py"
-assert (_this_dir / "models.py").exists(), "catalog/urls.py should have a sibling named models.py"
+assert not (_this_dir / "settings.py").exists(), (
+    "catalog/urls.py should NOT have a sibling named settings.py"
+)
+assert (_this_dir / "models.py").exists(), (
+    "catalog/urls.py should have a sibling named models.py"
+)
 
 from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("", views.index, name="index"),
 ]
